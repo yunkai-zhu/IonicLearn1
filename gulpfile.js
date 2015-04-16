752194bfd3b6bac65a09f600060fa1086eccf97e
祝云开 <zhuyk@lianchuang.com>
2015-04-16 16:48:09 +0800

    增加jshint js代码检查功能

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,24 @@ var gulp = require("gulp");
 del = require("del");
 uglify = require("gulp-uglify");
 concat = require("gulp-concat");
+jshint = require("gulp-jshint");
+stylish = require('jshint-stylish');
 
 gulp.task("clean",function(){
 	del("build");
 });
 
-
 gulp.task("minify",function(){
 	gulp.src("js/*.js")
 		.pipe(concat("all-mini.js"))
 		.pipe(uglify())
-		.pipe(gulp.dest("build"))
+		.pipe(gulp.dest("build"));
 });
 
-gulp.task("default",["clean","minify"])
+gulp.task("jshint",function(){
+  gulp.src("js/*.js")
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task("default",["clean","minify"]);
